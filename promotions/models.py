fb39e66f3f39355f36494c977a8018238a3c538a
Laurent Peuch <cortex@worlddomination.be>
2014-11-10 12:34:40 +0100

    [enh] add __unicode__ to professor and student

--- promotions/models.py
+++ promotions/models.py
@@ -5,12 +5,20 @@ from django.contrib.auth.models import User
 class Professor(models.Model):
     user = models.OneToOneField(User)
 
+    def __unicode__(self):
+        return ("%s %s" % (self.user.first_name, self.user.last_name)) if self.user.first_name or self.user.last_name else self.user.username
+
 
 class Student(models.Model):
     user = models.OneToOneField(User)
 
+    def __unicode__(self):
+        return ("%s %s" % (self.first_name, self.last_name)) if self.first_name or self.last_name else self.username
 
 class Lesson(models.Model):
     name = models.CharField(max_length=255)
     students = models.ManyToManyField(Student, null=True, blank=True)
     professors = models.ManyToManyField(Professor, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
