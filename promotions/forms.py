8f91c42e32e9747c22e34c434ea1aea6ea4e1f0d
Laurent Peuch <cortex@worlddomination.be>
2014-11-30 18:23:21 +0100

    [enh] yay, add form to add class

---
+++ promotions/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+
+from .models import Lesson
+
+
+class LessonForm(ModelForm):
+    class Meta:
+        model = Lesson
+        fields = ['name']
--- promotions/views.py
+++ promotions/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 
 from .models import Lesson
+from .forms import LessonForm
 
 
 def dashboard(request):
+    form = LessonForm(request.POST) if request.method == "POST" else LessonForm()
+
+    if form.is_valid():
+        lesson = form.save()
+        lesson.professors.add(request.user.professor)
+        return HttpResponseRedirect(reverse("professor_dashboard"))
+
     return render(request, "professor/dashboard.haml", {
-        "lessons": Lesson.objects.filter(professors=request.user.professor)
+        "lessons": Lesson.objects.filter(professors=request.user.professor),
+        "add_lesson_form": form,
     })
