c3526ac05c5b714c6aaf1ddfa8a041b50653cdd8
Laurent Peuch <cortex@worlddomination.be>
2014-11-30 18:03:48 +0100

    [enh] list classes on prof dashboard

--- promotions/views.py
+++ promotions/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Lesson
+
 
 def dashboard(request):
-    return render(request, "professor/dashboard.haml", {})
+    return render(request, "professor/dashboard.haml", {
+        "lessons": Lesson.objects.filter(professors=request.user.professor)
+    })
