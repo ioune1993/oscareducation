3d817128d38e727245376e6641cf333d52b53619
Laurent Peuch <cortex@worlddomination.be>
2014-11-30 16:21:31 +0100

    [enh] on root url, redirect to login

--- oscar/urls.py
+++ oscar/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.views.generic.base import RedirectView
+
+
 urlpatterns = patterns('',
     url(r'^auth/', include('authentification.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(pattern_name="login")),
 )
