e25c9589978f2fb769a12c6283c08d1e4c36a263
Laurent Peuch <cortex@worlddomination.be>
2014-11-30 19:24:35 +0100

    [mod] change way of importing forms module

--- promotions/forms.py
+++ promotions/forms.py
@@ -1,9 +1,9 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Lesson
 
 
-class LessonForm(ModelForm):
+class LessonForm(forms.ModelForm):
     class Meta:
         model = Lesson
         fields = ['name']
