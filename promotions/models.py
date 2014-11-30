50380c987589405bf6f52b7adacbf78441b596bb
Laurent Peuch <cortex@worlddomination.be>
2014-11-30 16:38:12 +0100

    [mod] pep8

--- promotions/models.py
+++ promotions/models.py
@@ -15,6 +15,7 @@ class Student(models.Model):
     def __unicode__(self):
         return ("%s %s" % (self.user.first_name, self.user.last_name)) if self.user.first_name or self.user.last_name else self.user.username
 
+
 class Lesson(models.Model):
     name = models.CharField(max_length=255)
     students = models.ManyToManyField(Student, null=True, blank=True)
