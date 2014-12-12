8e93857e7f08422fc9af0f8948bd9c57a995476c
Laurent Peuch <cortex@worlddomination.be>
2014-12-12 05:24:49 +0100

    [mod] update skill models to fit data

--- skills/models.py
+++ skills/models.py
@@ -4,9 +4,16 @@ from promotions.models import Student
 
 
 class Skill(models.Model):
+    code = models.CharField(max_length=20, unique=True, db_index=True)
+
     name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    level = models.PositiveIntegerField()
+    stage = models.CharField(max_length=255)
+    section = models.CharField(max_length=255)
+
     depend = models.ManyToManyField('Skill')
-    year = models.CharField(max_length=255)
 
 
 class StudentSkill(models.Model):
