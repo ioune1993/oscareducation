2344d960fc787ee114a158bb3d5834111df6983f
Laurent Peuch <cortex@worlddomination.be>
2014-12-13 09:43:03 +0100

    [enh] display StudentSkill instead of Skills  on student page

--- promotions/views.py
+++ promotions/views.py
@@ -70,7 +70,7 @@ def student_detail_view(request, pk):
 
     return render(request, "professor/student_detail_view.haml", {
         "student": student,
-        "skills": Skill.objects.order_by('-level', '-code'),
+        "skills": StudentSkill.objects.filter(student=student).order_by('-skill__level', '-skill__code'),
     })
 
 
