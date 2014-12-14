4abd22e584775d7d5226847d0959abb5d498bc19
Laurent Peuch <cortex@worlddomination.be>
2014-12-14 05:07:28 +0100

    [mod] pep8

--- authentification/urls.py
+++ authentification/urls.py
@@ -5,6 +5,7 @@
 
 from django.conf.urls import patterns, url
 
+
 urlpatterns = patterns('authentification.views',
     url(r'^login/$', 'login', name='login'),
     url(r'^logout/$', 'logout', name='logout'),
