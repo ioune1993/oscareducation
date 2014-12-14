d985fb41a8ad54472d52344d7fca9d563f928b08
Laurent Peuch <cortex@worlddomination.be>
2014-12-14 05:05:45 +0100

    [enh] redict on dashboard or on login on root in function of logged user

--- oscar/urls.py
+++ oscar/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from django.views.generic.base import RedirectView
 
-
-urlpatterns = patterns('',
+urlpatterns = patterns('oscar.views',
     url(r'^accounts/', include('authentification.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^professor/', include("promotions.urls")),
     url(r'^student/', include("student.urls")),
-    url(r'^$', RedirectView.as_view(pattern_name="login")),
+    url(r'^$', 'root_redirection'),
 )
---
+++ oscar/views.py
@@ -0,0 +1,15 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+
+def root_redirection(request):
+    if hasattr(request.user, "professor"):
+        return HttpResponseRedirect(reverse("professor_dashboard"))
+
+    if hasattr(request.user, "student"):
+        return HttpResponseRedirect(reverse("student_dashboard"))
+
+    if request.user.is_superuser:
+        return HttpResponseRedirect("/admin/")
+
+    return HttpResponseRedirect(reverse("login"))
