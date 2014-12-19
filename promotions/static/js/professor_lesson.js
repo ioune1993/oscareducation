cb26c13d32da6476cf4c8d60eee45f5e9ccefbeb
Laurent Peuch <cortex@worlddomination.be>
2014-12-19 12:55:22 +0100

    [enh] start to move test interface on lesson to angular

---
+++ promotions/static/js/professor_lesson.js
@@ -0,0 +1,17 @@
+function createTestController($scope) {
+    $scope.tests = [
+        {"name": "plop"},
+        {"name": "plop"},
+        {"name": "plop"},
+        {"name": "plop"},
+        {"name": "plop"},
+        {"name": "plop"},
+        {"name": "plop"},
+        {"name": "plop"},
+        {"name": "plop"}
+    ]
+
+    $scope.addNewTest = function() {
+        
+    }
+}
