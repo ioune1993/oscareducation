4c643ca1d37f0c0af3ff89bd395c3915af92f5e6
Laurent Peuch <cortex@worlddomination.be>
2014-12-19 20:28:51 +0100

    [enh] record when the test has been created

--- examinations/models.py
+++ examinations/models.py
@@ -8,6 +8,7 @@ class Test(models.Model):
     name = models.CharField(max_length=255)
     lesson = models.ForeignKey(Lesson)
     skills = models.ManyToManyField(Skill)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Exercice(models.Model):
