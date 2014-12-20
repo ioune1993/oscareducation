85c1fec9315fecd2206cb1899c0fd9c8c776ba60
Laurent Peuch <cortex@worlddomination.be>
2014-12-20 12:56:16 +0100

    [enh] sent lessons data to student dashboard template

--- student/views.py
+++ student/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from promotions.models import Lesson
+
 from utils import user_is_student
 
 
 @user_is_student
 def dashboard(request):
-    return render(request, "student/dashboard.haml", {})
+    return render(request, "student/dashboard.haml", {
+        "lessons": Lesson.objects.filter(students=request.user.student)
+    })
