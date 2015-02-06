1d8c7d0c7e67e713afeb6409bd99d83362d04553
Laurent Peuch <cortex@worlddomination.be>
2015-02-06 20:40:25 +0100

    [enh] button to start a test

--- student/urls.py
+++ student/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('student.views',
     url(r'^dashboard/$', 'dashboard', name='student_dashboard'),
     url(r'^test/(?P<pk>\d+)/$', 'pass_test', name='student_pass_test'),
+    url(r'^test/(?P<pk>\d+)/start/$', 'start_test', name='student_start_test'),
 )
--- student/views.py
+++ student/views.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 from django.shortcuts import render, get_object_or_404
 from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views.decorators.http import require_POST
 
 from examinations.models import TestStudent
 
@@ -20,4 +25,19 @@ def pass_test(request, pk):
 
     return render(request, "examinations/pass_test.haml", {
         "test": test_student.test,
+        "test_student": test_student,
     })
+
+
+@require_POST
+@user_is_student
+def start_test(request, pk):
+    test_student = get_object_or_404(TestStudent, pk=pk)
+
+    if test_student.student != request.user.student:
+        raise PermissionDenied()
+
+    test_student.started_at = datetime.now()
+    test_student.save()
+
+    return HttpResponseRedirect(reverse('student_pass_test', args=(test_student.pk,)))
