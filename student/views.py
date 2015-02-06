caa371bfbc4bf479d41fc8d8a9f785b68448b9a4
Laurent Peuch <cortex@worlddomination.be>
2015-02-06 18:24:20 +0100

    [mod] list teststudent instead of tests

--- student/views.py
+++ student/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 
-from promotions.models import Lesson
 
 from utils import user_is_student
 
 
 @user_is_student
 def dashboard(request):
-    return render(request, "student/dashboard.haml", {
-        "lessons": Lesson.objects.filter(students=request.user.student)
-    })
+    return render(request, "student/dashboard.haml", {})
