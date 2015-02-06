d9ca3ffe68d5b0423fd15b7a1c9041b71184d00a
Laurent Peuch <cortex@worlddomination.be>
2015-02-06 19:24:26 +0100

    [enh] start test page for student

--- student/urls.py
+++ student/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('student.views',
     url(r'^dashboard/$', 'dashboard', name='student_dashboard'),
+    url(r'^test/(?P<pk>\d+)/$', 'pass_test', name='student_pass_test'),
 )
--- student/views.py
+++ student/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.exceptions import PermissionDenied
 
+from examinations.models import TestStudent
 
 from utils import user_is_student
 
@@ -7,3 +9,15 @@ from utils import user_is_student
 @user_is_student
 def dashboard(request):
     return render(request, "student/dashboard.haml", {})
+
+
+@user_is_student
+def pass_test(request, pk):
+    test_student = get_object_or_404(TestStudent, pk=pk)
+
+    if test_student.student != request.user.student:
+        raise PermissionDenied()
+
+    return render(request, "examinations/pass_test.haml", {
+        "test": test_student.test,
+    })
