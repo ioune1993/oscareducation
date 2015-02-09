687a9a5b00397a66817c568e65850405eb9e4624
Laurent Peuch <cortex@worlddomination.be>
2015-02-09 23:36:01 +0100

    [fix] professor skills pages wern' access protected

--- promotions/urls.py
+++ promotions/urls.py
@@ -3,12 +3,13 @@ from django.views.generic import DetailView
 
 from skills.models import Skill
 
+from .utils import user_is_professor
 
 urlpatterns = patterns('promotions.views',
     url(r'^dashboard/$', 'dashboard', name='professor_dashboard'),
     url(r'^lesson/(?P<pk>\d+)/$', 'lesson_detail_view', name='professor_lesson_detail_view'),
     url(r'^student/(?P<pk>\d+)/$', 'student_detail_view', name='professor_student_detail_view'),
-    url(r'^skill/(?P<slug>\w+)/$', DetailView.as_view(model=Skill, slug_field="code", template_name="professor/skill_detail.haml"), name='professor_skill_detail_view'),
+    url(r'^skill/(?P<slug>\w+)/$', user_is_professor(DetailView.as_view(model=Skill, slug_field="code", template_name="professor/skill_detail.haml")), name='professor_skill_detail_view'),
     url(r'^regenerate_student_password/$', 'regenerate_student_password', name='professor_regenerate_student_password'),
 
     url(r'^validate_skill/(?P<student_skill>\d+)/$', 'validate_student_skill', name='professor_validate_student_skill'),
