ae79b465dae83ae63d75257de974f758c980a2ca
Laurent Peuch <cortex@worlddomination.be>
2015-02-09 19:06:37 +0100

    [enh] yay, load exercice content

--- examinations/management/commands/load_exercices.py
+++ examinations/management/commands/load_exercices.py
@@ -1,7 +1,10 @@
 import os
+import yaml
+import yamlordereddictloader
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print os.listdir("exercices")
+        for i in filter(lambda x: x.endswith((".yaml", ".yml")), os.listdir("exercices")):
+            print yaml.load(open("exercices/" + i), Loader=yamlordereddictloader.Loader)
