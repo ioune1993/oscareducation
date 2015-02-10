4601b4209b6187d2d9ede6238b4f09ba293cbf21
Laurent Peuch <cortex@worlddomination.be>
2015-02-10 15:01:52 +0100

    [enh] yay import exercices

--- examinations/management/commands/load_exercices.py
+++ examinations/management/commands/load_exercices.py
@@ -1,10 +1,41 @@
 import os
+
+import shutil
+
 import yaml
 import yamlordereddictloader
+
 from django.core.management.base import BaseCommand
+
+from skills.models import Skill
+from examinations.models import Exercice
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for i in filter(lambda x: x.endswith((".yaml", ".yml")), os.listdir("exercices")):
-            print yaml.load(open("exercices/" + i), Loader=yamlordereddictloader.Loader)
+            yaml.load(open("exercices/" + i), Loader=yamlordereddictloader.Loader)
+            skill_code = i.split(".")[0].split("_")[0]
+
+            skill = Skill.objects.get(code__iexact=skill_code)
+            file_name = i.split(".")[0]
+
+            if Exercice.objects.filter(file_name=file_name).exists():
+                print "updating", file_name, "..."
+                exercice = Exercice.objects.get(file_name=file_name)
+            else:
+                print "importing", file_name, "..."
+                exercice = Exercice(file_name=file_name)
+
+            exercice.skill = skill
+            exercice.answer = open("exercices/" + i).read()
+            exercice.content = open("exercices/" + file_name + ".html") if os.path.exists("exercices/" + file_name + ".html") else None,
+
+            exercice.save()
+
+        if not os.path.exists("examinations/static/exercices/"):
+            os.makedirs("examinations/static/exercices/")
+
+        for i in filter(lambda x: not x.endswith((".yaml", ".yml", ".html")) and not os.path.isdir("exercices/" + x), os.listdir("exercices")):
+            print "saving image", i, "..."
+            shutil.copyfile("exercices/" + i, "examinations/static/exercices/" + i)
