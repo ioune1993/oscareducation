3ad4f694ac0e634fe8d29d4dfb5f934ce68f907b
Laurent Peuch <cortex@worlddomination.be>
2015-02-13 00:56:00 +0100

    [enh] start to have pedagogic pages for skills

--- student/urls.py
+++ student/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('student.views',
     url(r'^dashboard/$', 'dashboard', name='student_dashboard'),
     url(r'^test/(?P<pk>\d+)/$', 'pass_test', name='student_pass_test'),
     url(r'^test/(?P<pk>\d+)/start/$', 'start_test', name='student_start_test'),
+    url(r'^skill/(?P<skill_code>[a-zA-Z0-9_-]+)/$', 'skill_pedagogic_ressources', name='student_skill_pedagogic_ressources'),
 )
