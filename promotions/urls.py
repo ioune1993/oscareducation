7aaa21821a4f34aa37f57d43588615789903cb76
Laurent Peuch <cortex@worlddomination.be>
2015-04-15 12:14:17 +0200

    [enh] create view to link to exercices list

--- promotions/urls.py
+++ promotions/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from skills.models import Skill
+from examinations.models import Exercice
 
 from .utils import user_is_professor
 
@@ -18,4 +19,6 @@ urlpatterns = patterns('promotions.views',
 
     url(r'^lesson_tests_and_skills/(?P<lesson_id>\d+).json$', 'lesson_tests_and_skills', name='lesson_tests_and_skills'),
     url(r'^add_test_for_lesson/$', 'add_test_for_lesson', name='add_test_for_lesson'),
+
+    url(r'^exercices/$', ListView.as_view(model=Exercice, template_name="promotions/exercice_list.haml"), name='professor_exercice_list'),
 )
