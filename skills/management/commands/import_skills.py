aafd6a38d0cf032deca3feee857edc7874977801
Laurent Peuch <cortex@worlddomination.be>
2015-04-21 03:05:02 +0200

    [enh] add debug output to import_skills

--- skills/management/commands/import_skills.py
+++ skills/management/commands/import_skills.py
@@ -19,8 +19,10 @@ class Command(BaseCommand):
             if Skill.objects.filter(code=row["Code"]).exists():
                 skill = Skill.objects.get(code=row["Code"])
                 skill.depends_on.clear()
+                print "update", row["Code"]
             else:
                 skill = Skill()
+                print "create", row["Code"]
                 skill.code = row["Code"]
 
             skill.name=row['Intitul\xc3\xa9']
@@ -35,6 +37,8 @@ class Command(BaseCommand):
                 dependancies.setdefault(row["Code"], []).append(next_)
 
         for key, value in filter(lambda (x, y): y, dependancies.iteritems()):
+            print "on", key
             skill = Skill.objects.get(code=key)
             for dep in value:
+                print " *", dep, "->", key
                 Skill.objects.get(code=dep).depends_on.add(skill)
