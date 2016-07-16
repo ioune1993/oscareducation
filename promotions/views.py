ee9ed77cb489f9c8ee11ac40eacd1c0221b5d2b5
Laurent Peuch <cortex@worlddomination.be>
2016-07-16 14:07:04 +0200

    [mod] start heatmap when we have information for one student

--- promotions/views.py
+++ promotions/views.py
@@ -65,7 +65,7 @@ def lesson_detail_view(request, pk):
             total = mastered + not_mastered
 
             # normally number_of_students will never be equal to 0 in this loop
-            if (float(total) / number_of_students) < 0.75:
+            if total == 0:
                 skill.heatmap_class = "mastered_not_enough"
                 continue
 
