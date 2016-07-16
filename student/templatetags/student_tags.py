7c65dc58fcacaa02b7fa6d334f7cc2fdf1b156e3
Karim Moulai <karim@dynaclic.be>
2016-07-16 12:53:36 +0200

    [enh] improved student tests listing and button to see previous tests

--- student/templatetags/student_tags.py
+++ student/templatetags/student_tags.py
@@ -3,6 +3,10 @@ from django import template
 register = template.Library()
 
 @register.filter
+def keep_unfinished_tests(teststudent_set_all):
+  return teststudent_set_all.filter(finished_at__isnull=True)
+
+@register.filter
 def strip_untested_skills(level_list, skip):
   if skip:
     return level_list
