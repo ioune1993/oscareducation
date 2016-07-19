02855fc9126e56a18fe2a6bfbb70404fbcf7d94a
Karim Moulai <karim@dynaclic.be>
2016-07-19 06:25:58 +0100

    [enh] improved code quality

--- student/templatetags/student_tags.py
+++ student/templatetags/student_tags.py
@@ -8,11 +8,4 @@ def keep_unfinished_tests(teststudent_set_all):
 
 @register.filter
 def strip_untested_skills(level_list):
-  # filter (remove) untested skills
-  filtered_level_list = []
-
-  for student_skill in level_list:
-    if student_skill.tested or student_skill.acquired:
-      filtered_level_list.append(student_skill)
-
-  return filtered_level_list
+  return [x for x in level_list if x.tested or x.acquired]
