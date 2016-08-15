c9dc298b8e00355b1da3bc8a4529cf779400a00c
Laurent Peuch <cortex@worlddomination.be>
2016-08-15 15:59:32 +0200

    [mod] pep8

--- student/templatetags/student_tags.py
+++ student/templatetags/student_tags.py
@@ -1,10 +1,12 @@
 from django import template
 
+
 register = template.Library()
 
 @register.filter
 def keep_unfinished_tests(teststudent_set_all):
   return teststudent_set_all.filter(finished_at__isnull=True)
+
 
 @register.filter
 def strip_untested_skills(level_list):
