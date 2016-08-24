916fab4598ba226deab2e4f0fc057f4cf8da5991
Laurent Peuch <cortex@worlddomination.be>
2016-08-24 06:07:09 +0200

    [fix] enforce ordering

--- promotions/templatetags/lesson_tags.py
+++ promotions/templatetags/lesson_tags.py
@@ -6,5 +6,5 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def get_students_skills(context, of_keyword, student, at_keyword, stage, as_keyword, target_name):
-    context[target_name] = StudentSkill.objects.filter(skill__in=stage.skills.all(), student=student).select_related("skill").order_by("skill__section")
+    context[target_name] = StudentSkill.objects.filter(skill__in=stage.skills.all(), student=student).select_related("skill").order_by("skill__section", "skill__code")
     return ""
