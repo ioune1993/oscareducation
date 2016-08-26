c158e98e94c94fc0bdec07e0ca80121ec8e27be2
Laurent Peuch <cortex@worlddomination.be>
2016-08-26 03:29:20 +0200

    [enh] start to do advanced checks for exercices

--- validate_exercices.py
+++ validate_exercices.py
@@ -7,9 +7,13 @@ def main():
 
     for yaml_file in filter(lambda x: x.endswith(".yaml"), os.listdir("exercices")):
         try:
-            yaml.safe_load(open(os.path.join("exercices", yaml_file)))
+            exercice = yaml.safe_load(open(os.path.join("exercices", yaml_file)))
         except Exception as e:
             print "Erreur: impossible de charger 'exercice/%s' car la syntaxe yaml n'est pas respectée: %s" % (yaml_file, e)
+            failed = True
+            continue
+
+        if not validate_exercice_yaml_structure(yaml_file, exercice):
             failed = True
 
     if not failed:
@@ -17,6 +21,14 @@ def main():
         sys.exit(0)
     else:
         sys.exit(1)
+
+
+def validate_exercice_yaml_structure(name, exercice):
+    if not isinstance(exercice, dict):
+        print "Error: le premier niveau d'indentation (zero) 'exercice/%s' doit être une série de chaînes de caractères se terminant par des ':'" % (name)
+        return False
+
+    return True
 
 
 if __name__ == '__main__':
