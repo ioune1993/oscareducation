a7b9a36ed22403777534cc32fc1bc85601202691
Laurent Peuch <cortex@worlddomination.be>
2016-09-26 17:11:21 +0200

    [fix] import khan video in french

--- skills/management/commands/import_khan_csv.py
+++ skills/management/commands/import_khan_csv.py
@@ -1,4 +1,6 @@
 import csv
+import requests
+from dateutil.parser import parse
 from django.core.management.base import BaseCommand
 
 from skills.models import KhanAcademyVideoReference
@@ -21,12 +23,28 @@ class Command(BaseCommand):
             else:
                 khan = KhanAcademyVideoReference(youtube_id=row["fr"])
 
-            khan.subject = row["Subject"]
-            khan.topic = row["Topic"]
-            khan.tutorial = row["Tutorial"]
-            khan.title = row["title"]
-            khan.slug = row["slug"]
-            khan.duration = row["duration"]
-            khan.fr_date_added = row["fr_date_added"] if row["fr_date_added"] else None
+
+            khan.subject = (row["Subject FR"] if row["Subject FR"] else row["Subject"]).strip()
+            khan.topic = (row["Topic FR"] if row["Topic FR"] else row["Topic"]).strip()
+            khan.tutorial = (row["Tutorial FR"] if row["Tutorial FR"] else row["Tutorial"]).strip()
+            khan.slug = row["slug"].strip()
+            khan.duration = row["duration"].strip()
+            khan.fr_date_added = parse(row["fr_date_added"]) if row["fr_date_added"] else None
+
+            if not khan.title:
+                if row["Title FR"]:
+                    khan.title = row["Title FR"].strip()
+                    print "csv", row["fr"], [khan.title]
+                else:
+                    content = requests.get("http://fr.khanacademy.org/api/v1/videos/%s" % row["fr"])
+                    if content.content == "null":
+                        if row["title"]:
+                            khan.title = row["title"].strip()
+                            print "csv", row["fr"], [khan.title]
+                        else:
+                            continue
+                    else:
+                        khan.title = content.json()["translated_title"].strip()
+                        print "api", row["fr"], khan.title
 
             khan.save()
