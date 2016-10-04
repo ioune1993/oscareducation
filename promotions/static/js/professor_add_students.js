f6d7b660b957c0240915ea5b7ba9c8d6e1b8f7bf
Laurent Peuch <cortex@worlddomination.be>
2016-10-04 16:13:56 +0200

    [enh] can add new student entry

--- promotions/static/js/professor_add_students.js
+++ promotions/static/js/professor_add_students.js
@@ -1,3 +1,9 @@
 function severalStudentsController($scope) {
     $scope.students = [0, 1];
+
+    $scope.addAnotherStudent = function() {
+        var new_number = $scope.students[$scope.students.length - 1] + 1;
+
+        $scope.students.push(new_number);
+    }
 }
