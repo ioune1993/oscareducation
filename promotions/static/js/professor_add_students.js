3bded114daec5d5b7be11fc5da37a14ebc597e42
Laurent Peuch <cortex@worlddomination.be>
2016-10-04 16:39:11 +0200

    [enh] can remove students from add student page list

--- promotions/static/js/professor_add_students.js
+++ promotions/static/js/professor_add_students.js
@@ -2,8 +2,16 @@ function severalStudentsController($scope) {
     $scope.students = [0, 1];
 
     $scope.addAnotherStudent = function() {
-        var new_number = $scope.students[$scope.students.length - 1] + 1;
+        if ($scope.students.length > 0) {
+            var new_number = $scope.students[$scope.students.length - 1] + 1;
+        } else {
+            var new_number = 0;
+        }
 
         $scope.students.push(new_number);
+    }
+
+    $scope.removeStudent = function(id) {
+        $scope.students.splice($scope.students.indexOf(id), 1);
     }
 }
