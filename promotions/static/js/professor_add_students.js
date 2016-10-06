92c4f6b49b279e9331185a00d2c708b08ef7e0e1
Laurent Peuch <cortex@worlddomination.be>
2016-10-06 12:22:12 +0200

    [enh] can add more students at once

--- promotions/static/js/professor_add_students.js
+++ promotions/static/js/professor_add_students.js
@@ -1,14 +1,16 @@
 function severalStudentsController($scope) {
     $scope.students = [0, 1];
 
-    $scope.addAnotherStudent = function() {
+    $scope.addMoreStudent = function(number) {
         if ($scope.students.length > 0) {
             var new_number = $scope.students[$scope.students.length - 1] + 1;
         } else {
             var new_number = 0;
         }
 
-        $scope.students.push(new_number);
+        for (var i = 0; i < number; ++i) {
+            $scope.students.push(new_number + i);
+        }
     }
 
     $scope.removeStudent = function(id) {
