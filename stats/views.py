9879e1a6a007764c8a75896465a2e4e0918f54f3
Laurent Peuch <cortex@worlddomination.be>
2016-10-10 11:01:29 +0200

    [enh] display latest added ressources

--- stats/views.py
+++ stats/views.py
@@ -4,7 +4,7 @@ from django.db.models import Count
 from .utils import user_is_superuser
 
 from promotions.models import Professor, Student, Lesson
-from skills.models import Skill
+from skills.models import Skill, KhanAcademyVideoSkill, SesamathSkill
 
 
 @user_is_superuser
@@ -16,4 +16,6 @@ def dashboard(request):
         "skills": Skill.objects.all(),
         "skills_with_khan_ressources": Skill.objects.annotate(Count('khanacademyvideoskill')).filter(khanacademyvideoskill__count__gt=0),
         "skills_with_sesamath_ressources": Skill.objects.annotate(Count('sesamathskill')).filter(sesamathskill__count__gt=0),
+        "khanacademyvideoskill": KhanAcademyVideoSkill.objects.order_by('-created_at'),
+        "sesamathskill": SesamathSkill.objects.order_by('-created_at'),
     })
