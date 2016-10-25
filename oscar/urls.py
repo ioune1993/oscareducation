1893fe8e53c231df9c41c32cf58bfe5810f28c4d
Laurent Peuch <cortex@worlddomination.be>
2016-10-25 17:35:11 +0200

    [fix] media files weren't working

--- oscar/urls.py
+++ oscar/urls.py
@@ -1,12 +1,15 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
+from . import views
 
-urlpatterns = patterns('oscar.views',
+urlpatterns = [
     url(r'^accounts/', include('authentification.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^professor/', include("promotions.urls", namespace="professor")),
     url(r'^student/', include("student.urls")),
     url(r'^stats/', include("stats.urls", namespace="stats")),
-    url(r'^$', 'root_redirection', name="home"),
-)
+    url(r'^$', views.root_redirection, name="home"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
