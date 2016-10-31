f17eab4b0304512c0e0c1e61423aba908c8869fb
Laurent Peuch <cortex@worlddomination.be>
2016-10-31 11:22:02 +0100

    [mod] DRY

--- promotions/static/js/professor_update_pedagogical_resources.js
+++ promotions/static/js/professor_update_pedagogical_resources.js
@@ -1,83 +1,27 @@
-function personal_resourceController($scope) {
-    $scope.links = [];
-    $scope.files = [];
+cloneController = function() {
+    return function personal_resourceController($scope) {
+        $scope.links = [];
+        $scope.files = [];
 
-    $scope.addMore = function(kind, number) {
-        if ($scope[kind].length > 0) {
-            var new_number = $scope[kind][$scope[kind].length - 1] + 1;
-        } else {
-            var new_number = 0;
+        $scope.addMore = function(kind, number) {
+            if ($scope[kind].length > 0) {
+                var new_number = $scope[kind][$scope[kind].length - 1] + 1;
+            } else {
+                var new_number = 0;
+            }
+
+            for (var i = 0; i < number; ++i) {
+                $scope[kind].push(new_number + i);
+            }
         }
 
-        for (var i = 0; i < number; ++i) {
-            $scope[kind].push(new_number + i);
+        $scope.remove = function(kind, id) {
+            $scope[kind].splice($scope[kind].indexOf(id), 1);
         }
-    }
-
-    $scope.remove = function(kind, id) {
-        $scope[kind].splice($scope[kind].indexOf(id), 1);
     }
 }
 
-function lesson_resourceController($scope) {
-    $scope.links = [];
-    $scope.files = [];
-
-    $scope.addMore = function(kind, number) {
-        if ($scope[kind].length > 0) {
-            var new_number = $scope[kind][$scope[kind].length - 1] + 1;
-        } else {
-            var new_number = 0;
-        }
-
-        for (var i = 0; i < number; ++i) {
-            $scope[kind].push(new_number + i);
-        }
-    }
-
-    $scope.remove = function(kind, id) {
-        $scope[kind].splice($scope[kind].indexOf(id), 1);
-    }
-}
-
-function exercice_resourceController($scope) {
-    $scope.links = [];
-    $scope.files = [];
-
-    $scope.addMore = function(kind, number) {
-        if ($scope[kind].length > 0) {
-            var new_number = $scope[kind][$scope[kind].length - 1] + 1;
-        } else {
-            var new_number = 0;
-        }
-
-        for (var i = 0; i < number; ++i) {
-            $scope[kind].push(new_number + i);
-        }
-    }
-
-    $scope.remove = function(kind, id) {
-        $scope[kind].splice($scope[kind].indexOf(id), 1);
-    }
-}
-
-function other_resourceController($scope) {
-    $scope.links = [];
-    $scope.files = [];
-
-    $scope.addMore = function(kind, number) {
-        if ($scope[kind].length > 0) {
-            var new_number = $scope[kind][$scope[kind].length - 1] + 1;
-        } else {
-            var new_number = 0;
-        }
-
-        for (var i = 0; i < number; ++i) {
-            $scope[kind].push(new_number + i);
-        }
-    }
-
-    $scope.remove = function(kind, id) {
-        $scope[kind].splice($scope[kind].indexOf(id), 1);
-    }
-}
+personal_resourceController = cloneController()
+lesson_resourceController = cloneController()
+exercice_resourceController = cloneController()
+other_resourceController = cloneController()
