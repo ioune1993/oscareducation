00fc65ffaf6ba9de6d5b67e91e18fe0da91a5788
Laurent Peuch <cortex@worlddomination.be>
2016-11-24 11:24:19 +0100

    [enh] return variable list

--- examinations/generation.py
+++ examinations/generation.py
@@ -1,5 +1,11 @@
 import re
 
+VARIABLES_REGEX = r"{ *([a-zA-Z-_]+) *}"
+
 
 def needs_to_be_generated(exercice_body):
-    return bool(re.search(r"{ *[a-zA-Z-_] *}", exercice_body))
+    return bool(re.search(VARIABLES_REGEX, exercice_body))
+
+
+def get_variable_list(exercice_body):
+    return {x: None for x in re.findall(VARIABLES_REGEX, exercice_body)}
--- examinations/tests.py
+++ examinations/tests.py
@@ -1,4 +1,4 @@
-from generation import needs_to_be_generated
+from generation import needs_to_be_generated, get_variable_list
 
 
 def test_detection_empty():
@@ -7,3 +7,19 @@ def test_detection_empty():
 
 def test_detection_one_variable():
     assert needs_to_be_generated("{a}")
+
+
+def test_get_variable_list_empty():
+    assert get_variable_list("") == {}
+
+
+def test_get_variable_list_one():
+    assert get_variable_list("{a}") == {"a": None}
+
+
+def test_get_variable_list_more():
+    assert get_variable_list("{a} {b}") == {"a": None, "b": None}
+
+
+def test_get_variable_list_long():
+    assert get_variable_list("{pouet}") == {"pouet": None}
