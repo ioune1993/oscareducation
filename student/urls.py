ff2fefdfee799c5391d55fe5cbebdc4bd28e1511
Laurent Peuch <cortex@worlddomination.be>
2016-12-13 14:17:47 +0100

    [enh] reuse ressource page of teacher for student

--- student/urls.py
+++ student/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns('student.views',
     url(r'^test/finished/$', TemplateView.as_view(template_name="student/test/list_finished.haml"), name='student_test_finished'),
     url(r'^test/(?P<pk>\d+)/$', 'pass_test', name='student_pass_test'),
     url(r'^test/(?P<pk>\d+)/start/$', 'start_test', name='student_start_test'),
-    url(r'^skill/(?P<slug>[a-zA-Z0-9_-]+)/$', user_is_student(DetailView.as_view(model=Skill, slug_field="code", template_name="skills/skills_detail.haml")), name='student_skill_pedagogic_ressources'),
+    url(r'^skill/(?P<slug>[a-zA-Z0-9_-]+)/$', user_is_student(DetailView.as_view(model=Skill, slug_field="code", template_name="professor/skill/update_pedagogical_resources.haml")), name='student_skill_pedagogic_ressources'),
 )
