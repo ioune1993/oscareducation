5507a3277951087c98e433d3a568c3340e85fb5f
Laurent Peuch <cortex@worlddomination.be>
2017-01-19 16:57:54 +0100

    [enh] validate math questions

--- examinations/utils.py
+++ examinations/utils.py
@@ -15,8 +15,8 @@ def validate_exercice_yaml_structure(exercice):
         if "type" not in data:
             return (u"chaque question doit avoir un type, or la question '%s' n'a pas de type" % (question)).encode("Utf-8")
 
-        if data["type"] not in ("radio", "text", "checkbox"):
-            return (u"la question '%s' possède un type invalide: '%s'\nLes types valides sont : 'text', 'checkbox' et 'radio' " % (question, data["type"])).encode("Utf-8")
+        if data["type"] not in ("radio", "text", "checkbox", "math"):
+            return (u"la question '%s' possède un type invalide: '%s'\nLes types valides sont : 'text', 'checkbox', 'math' et 'radio' " % (question, data["type"])).encode("Utf-8")
 
         if "answers" not in data:
             return (u"chaque question doit avoir une section 'answers' contenant les réponses, or la question '%s' ne contient pas cette section" % (question)).encode("Utf-8")
@@ -56,7 +56,7 @@ def validate_exercice_yaml_structure(exercice):
             if number_of_true == 0:
                 return(u"une question de type checkbox doit avoir au moins une réponse de correcte, or la question '%s' n'a pas de réponse correcte possible" % (question)).encode("Utf-8")
 
-        elif data["type"] == "text":
+        elif data["type"] in ("text", "math"):
             if not isinstance(data["answers"], list):
                 return (u"le contenu des réponses d'une question de type text doit être une série de chaînes de caractères précédé par '- ' (une liste), or les réponses de la question '%s' ne sont pas sous forme d'une liste" % (question)).encode("Utf-8")
 
