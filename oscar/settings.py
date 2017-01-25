a249e4b1a4e10162687f41199ece5264a66801ef
Laurent Peuch <cortex@worlddomination.be>
2017-01-25 12:20:31 +0100

    [enh] log errors on the shell

--- oscar/settings.py
+++ oscar/settings.py
@@ -107,6 +107,24 @@ DATABASES = {
     }
 }
 
+# https://docs.djangoproject.com/en/1.10/topics/logging/
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
