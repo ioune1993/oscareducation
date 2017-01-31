c578627c8ed3330039b958eb31c0fdc8eb3304f3
Laurent Peuch <cortex@worlddomination.be>
2017-01-31 17:38:35 +0100

    [enh] continue to work on keyboard

--- oscar/static/js/base.js
+++ oscar/static/js/base.js
@@ -43,10 +43,10 @@ MATH_CUSTOM_LAYOUT = {
     },
     "math-advanced": {
         'default': [
-            '7 8 9 frac  {b}  {sp:1} <      leq    geq     >',
-            '4 5 6 times sqrt {sp:1} x      y      u       cos',
-            '1 2 3 -     ^    {sp:1} neq     {Up}   =      sin',
-            '0 ( ) +     ,    {sp:1} {left} {Down} {right} tan'
+            '7 8 9 frac  {b}  {sp:1} <      leq    geq     >   {sp:1} pi',
+            '4 5 6 times sqrt {sp:1} x      y      u       cos {sp:1} %',
+            '1 2 3 -     ^    {sp:1} neq     {Up}   =      sin {sp:1} int',
+            '0 ( ) +     ,    {sp:1} {left} {Down} {right} tan {sp:1} _'
 
             // '()',
             // 'x y n t u',
