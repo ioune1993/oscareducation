65c3d227786ba1a21ad1a7f002d39e7974aec2c2
Laurent Peuch <cortex@worlddomination.be>
2017-02-01 15:11:01 +0100

    [mod] refactor, DRY on mathquill keyboard

--- promotions/static/js/professor_validate_exercice.js
+++ promotions/static/js/professor_validate_exercice.js
@@ -170,37 +170,8 @@ function validateExerciceController($scope, $http, $sce, $timeout, $location) {
     }
 
     $scope.renderMathquil = function(topIndex, question) {
-        console.log("renderMathquil, topIndex: " + topIndex);
-        var MQ = MathQuill.getInterface(2);
-
-        var composedKeys = {
-            cos: ["cos", "(", ")", "left"],
-            sin: ["sin", "(", ")", "left"],
-            tan: ["tan", "(", ")", "left"],
-            log: ["log", "(", ")", "left"],
-            log_: ["log", "_", "right", "(", ")", "left", "left", "left"],
-            ln: ["ln", "(", ")", "left"],
-            "()": ["(", ")", "left"],
-        }
-
-        var specialKeys = {
-            right: "Right",
-            left: "Left",
-            Down: "Down",
-            Up: "Up",
-            bksp: "Backspace",
-            tab: "Tab"
-        }
-
-        // add special keys, but don't override previous keyaction definitions
-        Object.keys(specialKeys).forEach(function(key){
-            if (!$.keyboard.keyaction[key]) {
-                $.keyboard.keyaction[key] = specialKeys[key];
-            }
-        });
-
-        console.log($(".mathquill-" + topIndex));
-        $(".mathquill-" + topIndex).each(function(index, mq) {
+        console.log("pouet");
+        renderMathquil($(".mathquill-" + topIndex), function(MQ, index, mq) {
             var mathquill = MQ.MathField(mq, {
                 handlers: {
                     edit: function() {
@@ -216,46 +187,7 @@ function validateExerciceController($scope, $http, $sce, $timeout, $location) {
 
             var keyboard = $($(mq).parent().children()[0]);
 
-            $(mq).click(function(){
-                // keyboard.getkeyboard().reveal();
-                keyboard.getkeyboard().reveal();
-            })
-
-            keyboard
-                .on('keyboardChange', function(e, keyboard, el) {
-                    console.log(e.action);
-                    if (specialKeys[e.action]) {
-                        mathquill.keystroke(specialKeys[e.action]);
-                    } else if (composedKeys[e.action]) {
-                        $(composedKeys[e.action]).each(function(index, value) {
-                            if (specialKeys[value]) {
-                                mathquill.keystroke(specialKeys[value]);
-                            } else {
-                                mathquill.cmd(value);
-                            }
-                        });
-                    } else {
-                        mathquill.cmd(e.action);
-                    }
-                    // $('#mathquill').focus();
-                })
-            .keyboard({
-                usePreview: false,
-                lockInput: true,
-                noFocus: true,
-                layout: 'custom',
-                display: {
-                    "Down": "&darr;",
-                    "Up": "&uarr;"
-                },
-                customLayout: MATH_CUSTOM_LAYOUT[$(mq).attr("data-keyboard-type")],
-                useCombos: false
-            })
-            // activate the typing extension
-            .addTyping({
-                showTyping: true,
-                delay: 250
-            });
+            return [mathquill, keyboard];
         });
     }
 
