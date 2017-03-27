282f39a9e90850b8a89f7324369289bcc77bf569
Laurent Peuch <cortex@worlddomination.be>
2017-03-27 12:58:32 +0200

    [fix] encoding on sesamath

--- promotions/utils.py
+++ promotions/utils.py
@@ -10,3 +10,22 @@ def generate_random_password(size):
 
 def user_is_professor(function):
     return user_passes_test(lambda x: hasattr(x, "professor"))(function)
+
+
+def force_encoding(string):
+    try:
+        return string.encode("Utf-8")
+    except UnicodeDecodeError:
+        pass
+
+    try:
+        return string.decode("Utf-8")
+    except UnicodeDecodeError:
+        pass
+
+    try:
+        return string.encode("latin")
+    except UnicodeDecodeError:
+        pass
+
+    return string.decode("latin")
