e9a78b9dd67e201720c499535ea3b7e4d8a85874
Laurent Peuch <cortex@worlddomination.be>
2017-03-28 18:21:48 +0200

    [enh] start testing answers with 'text'

--- examinations/management/commands/check_exercices.py
+++ examinations/management/commands/check_exercices.py
@@ -12,3 +12,26 @@ class Command(BaseCommand):
             from ipdb import launch_ipdb_on_exception
             with launch_ipdb_on_exception():
                 exercice.check_answers({})
+
+        for exercice in Exercice.objects.filter(testable_online=True):
+            with launch_ipdb_on_exception():
+                for number, (question, answers) in enumerate(exercice.get_questions().items()):
+                    if answers["type"] == "text":
+                        for answer in answers["answers"]:
+                            assert exercice.check_answers({
+                                str(number): answer
+                            })["answers"][number]["correct"] == True
+                    elif answers["type"] == "checkbox":
+                        pass
+                    elif answers["type"] == "graph":
+                        pass
+                    elif answers["type"] == "radio":
+                        pass
+                    elif answers["type"] == "math-simple":
+                        pass
+                    elif answers["type"] == "math-advanced":
+                        pass
+                    else:
+                        raise Exception("Exercice %s answers type is wrong: %s" % (exercice, answers.get("type")))
+
+        from ipdb import set_trace; set_trace()
