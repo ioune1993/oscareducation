c7ba8ffc04a0eeb3b2f6a82b7941d5c34d019685
Laurent Peuch <cortex@worlddomination.be>
2017-04-27 14:13:40 +0200

    [enh] no top menu on homepage

--- promotions/views.py
+++ promotions/views.py
@@ -40,6 +40,7 @@ from .utils import generate_random_password, user_is_professor, force_encoding
 def dashboard(request):
     return render(request, "professor/dashboard.haml", {
         "lessons": Lesson.objects.filter(professors=request.user.professor).annotate(Count("students")).select_related("stage"),
+        "no_menu": True,
     })
 
 
