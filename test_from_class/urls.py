24a3319a2bd1919b0e56d52bd0384763963494ee
bclasse <claeys.bastien@skynet.be>
2017-08-22 15:04:09 +0200

    Fix TestFromClass and syntax for KhanAcademy

--- test_from_class/urls.py
+++ test_from_class/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     url(r'^lesson/(?P<pk>\d+)/test/from-class/add/$', views.lesson_test_from_class_add, name='lesson_test_from_class_add'),
     url(r'^lesson/(?P<lesson_pk>\d+)/test/from-class/(?P<pk>\d+)/fill/$', views.lesson_test_from_class_fill, name='lesson_test_from_class_fill'),
     url(r'^lesson/(?P<lesson_pk>\d+)/test/from-class/(?P<pk>\d+)/$', user_is_professor(TestFromClassDetailView.as_view()), name='lesson_test_from_class_detail'),
+    url(r'^lesson/(?P<lesson_pk>\d+)/test/from-class/(?P<pk>\d+)/modify/$',
+        user_is_professor(TestFromClassDetailView.as_view(template_name="professor/lesson/test/from-class/exercices.haml")),
+        name='lesson_test_from_class_modify'),
     url(r'^add_test_from_class_for_lesson/$', views.lesson_test_from_class_add_json, name='lesson_test_from_class_add_json'),
+
 ]
