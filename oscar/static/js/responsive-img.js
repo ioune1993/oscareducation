becc07bffd0bdf4f8ca4c6d657e815206a132a37
chems <chemstudio2@gmail.com>
2017-08-25 14:25:59 +0200

    remove responsive image

--- oscar/static/js/responsive-img.js
+++ oscar/static/js/responsive-img.js
@@ -2,7 +2,7 @@ $(function(){
 
 //get all img tag and class img-responsive if not exist
 
-    if ( !$('img').hasClass('img-responsive') ) {
+   /* if ( !$('img').hasClass('img-responsive') ) {
 
         $('img').addClass('img-responsive')
 
